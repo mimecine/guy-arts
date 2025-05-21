85e839915164463650d51e51657cd407bd265df7
Marco Romeny <marco@mimecom.net>
2025-05-21 17:27:18 +0100

    Cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,8 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()]
-  },
-
-  // adapter: netlify()
-  integrations: [react()],
-
-  adapter: cloudflare()
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
