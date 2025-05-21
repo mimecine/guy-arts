3c979153a9ab185967ff0a47376cc8b64e254f44
Marco Romeny <marco@mimecom.net>
2025-05-21 12:49:38 +0100

    Added posts to test out "Pages CMS"

--- src/content.config.ts
+++ src/content.config.ts
@@ -43,7 +43,22 @@ const pages = defineCollection({
         })
 });
 
+const posts = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.md', base: './src/content/posts' }),
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            image: image()
+                .transform((image) => {
+                    return unescape(image); // transforms image __ASTRO_IMAGE_/src/assets/An%20Image.jpg to image __ASTRO_IMAGE_/src/assets/An Image.jpg
+                })
+                .optional()
+                .nullish()
+        })
+});
+
 export const collections = {
     things,
-    pages
+    pages,
+    posts
 };
